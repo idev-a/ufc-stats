73f387a0ebe57053b18b57e06b260a8cf3c45d9c
idev-a <ideveloper003@protonmail.com>
2021-01-26 09:47:13 -0800

    fix cross domain

--- fighter/fighter/settings.py
+++ fighter/fighter/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = 'u1jy5r$$5xc2d1=1ckb#ndnn2h99b2b!1xd9cw-@0_5nsttt@y'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = ['localhost', '3.140.186.130', 'beta.fightquake.com', 'fightquake.com']
+ALLOWED_HOSTS = ['localhost', '3.140.186.130', 'beta.fightquake.com', 'fightquake.com', 'www.fightquake.com']
 
 DATABASES = { 'default' : dj_database_url.config(default=config('DATABASE_URL'))}
 
@@ -184,6 +184,7 @@ CORS_ALLOWED_ORIGINS = [
     'http://localhost:8085',
     'http://localhost:8000',
     'http://beta.fightquake.com',
+    'http://fightquake.com',
     'https://fightquake.com'
 ]
 
