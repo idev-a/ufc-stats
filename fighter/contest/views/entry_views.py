b1472112da1a9e9bdc22a51639dcb15ab92c85b1
idev-a <ideveloper003@protonmail.com>
2021-03-25 09:00:09 -0700

    api

--- fighter/contest/views/entry_views.py
+++ fighter/contest/views/entry_views.py
@@ -591,7 +591,6 @@ class EntryViewSet(NestedViewSetMixin, viewsets.ModelViewSet):
             entry_serializer = None
             is_exist = False
             message = 'Successfully done.'
-            pdb.set_trace()
             try:
                 if int(data['game']) != -1:
                     entry = Entry.objects.get(event_id=data['event'], user_id=data['user'], game_id=data['game'], entry_number=data['entry_number'])
