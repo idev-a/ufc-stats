615d3b8287cf637e1fce83c239f91fbaa909a73d
idev-a <ideveloper003@protonmail.com>
2021-03-25 04:26:04 -0700

    update game

--- fighter/contest/commons.py
+++ fighter/contest/commons.py
@@ -22,50 +22,52 @@ from contest.serializers import (
 
 def get_games(event, user_id=None):
     _ = Game.objects.filter(event=event).filter(type_of_registration='public').filter(buyin=0).first()
-    event_data = EventSerializer(event).data
-    games = [dict(
-        name=_.name,
-        group='single',
-        date=_.date,
-        value=_.id,
-        event=event_data,
-        type_of_registration=_.type_of_registration,
-        instructions=_.instructions,
-        summary=_.summary,
-        rules_set=_.rules_set,
-        action=_.action,
-        genre=_.genre,
-        buyin=_.buyin,
-        prize=_.prize,
-        joined_users=UserSerializer(_.joined_users, many=True).data,
-        added_prizepool=_.added_prizepool,
-        re_entry=_.re_entry,
-        multientry=_.multientry
-    )]
+    games = []
+    if _:
+	    event_data = EventSerializer(event).data
+    	games.append(dict(
+	        name=_.name,
+	        group='single',
+	        date=_.date,
+	        value=_.id,
+	        event=event_data,
+	        type_of_registration=_.type_of_registration,
+	        instructions=_.instructions,
+	        summary=_.summary,
+	        rules_set=_.rules_set,
+	        action=_.action,
+	        genre=_.genre,
+	        buyin=_.buyin,
+	        prize=_.prize,
+	        joined_users=UserSerializer(_.joined_users, many=True).data,
+	        added_prizepool=_.added_prizepool,
+	        re_entry=_.re_entry,
+	        multientry=_.multientry
+	    ))
 
-    if user_id:
-        multi_games = Game.objects.filter(joined_users__pk=user_id).filter(event=event)
-        if multi_games:
-            for _ in multi_games:
-                games.append(dict(
-                    name=_.name,
-                    group='Multiple',
-                    date=_.date,
-                    value=_.id,
-                    event=event_data,
-                    type_of_registration=_.type_of_registration,
-                    instructions=_.instructions,
-                    summary=_.summary,
-                    rules_set=_.rules_set,
-                    action=_.action,
-                    genre=_.genre,
-                    buyin=_.buyin,
-                    prize=_.prize,
-                    joined_users=UserSerializer(_.joined_users, many=True).data,
-                    added_prizepool=_.added_prizepool,
-                    re_entry=_.re_entry,
-                    multientry=_.multientry
-                ))
+	    if user_id:
+	        multi_games = Game.objects.filter(joined_users__pk=user_id).filter(event=event)
+	        if multi_games:
+	            for _ in multi_games:
+	                games.append(dict(
+	                    name=_.name,
+	                    group='Multiple',
+	                    date=_.date,
+	                    value=_.id,
+	                    event=event_data,
+	                    type_of_registration=_.type_of_registration,
+	                    instructions=_.instructions,
+	                    summary=_.summary,
+	                    rules_set=_.rules_set,
+	                    action=_.action,
+	                    genre=_.genre,
+	                    buyin=_.buyin,
+	                    prize=_.prize,
+	                    joined_users=UserSerializer(_.joined_users, many=True).data,
+	                    added_prizepool=_.added_prizepool,
+	                    re_entry=_.re_entry,
+	                    multientry=_.multientry
+	                ))
 
     return games
 
